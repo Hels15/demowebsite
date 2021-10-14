3be5f725f525579b2647f251e35ed9359a7296be
Hadházy Tamás <hadhazytamas15@gmail.com>
2021-10-14 17:11:48 +0200

    update

--- pages/models.py
+++ pages/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
-# Create your models here.
-# class MyPage(models.Model):
-#     title = models.CharField()
+
+class MyPage(models.Model):
+     title = models.CharField(max_length=200)
+     subtitle = models.CharField(max_length=200)
+     email = models.EmailField(max_length=200)
+     phone = models.CharField(max_length=200)
+     github = models.URLField(max_length=200, blank=True)
